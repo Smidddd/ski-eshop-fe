78d0133abbb1925b6b94f581212b5a2d0c60e31c
smidd <smido209@gmail.com>
2023-05-09 19:59:33 +0200

    inventory request fix

--- src/app/common/service/inventory.service.ts
+++ src/app/common/service/inventory.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Product} from "../model/product.model";
 import {InventoryModel} from "../model/inventory.model";
+import {invIds} from "../model/invIds.model";
 
 @Injectable({
   providedIn: 'root'
@@ -15,10 +16,8 @@ export class InventoryService {
   searchInventory(): Observable<InventoryModel[]> {
     return this.http.get<InventoryModel[]>(this.url);
   }
-  getItemsByIds(inventoryIds: Array<number>): Observable<InventoryModel[]> {
-    let params = new HttpParams();
-    params.appendAll({'ids': inventoryIds})
-    return this.http.get<InventoryModel[]>(`${this.url}/byId`,{params: params});
+  getItemsByIds(inventoryIds: invIds): Observable<InventoryModel[]> {
+    return this.http.post<InventoryModel[]>(`${this.url}/byId`, inventoryIds);
   }
   getItem(inventoryId: number): Observable<InventoryModel> {
     return this.http.get<InventoryModel>(`${this.url}/${inventoryId}`);
